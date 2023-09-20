87863d9e1ee6f6ea70df95dc4ea380e54f116b74
Bruno G Carvalho <113460204+brunogomescarvalho@users.noreply.github.com>
2023-09-20 15:49:50 -0300

    first commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use('api/', router)
+server.use('/api', router)
 
 const porta = process.env.PORT || 3000
 server.listen(porta, () => {
